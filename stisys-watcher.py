033a38131ef52f9c944e7480eb7fbab8a5159fa5
Marcel Röthke <marcel@roethke.info>
2018-01-30 23:13:37 +0100

    refactor

--- stisys-watcher.py
+++ stisys-watcher.py
@@ -1,23 +1,32 @@
-import requests
-import getpass
-from bs4 import BeautifulSoup
+#!/usr/bin/env python3
+
 import argparse
+import getpass
+
+from os import path
+from urllib.parse import urljoin
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+STISYS_BASE_URL = 'https://stisys.haw-hamburg.de'
+STISYS_LOGIN_URL = urljoin(STISYS_BASE_URL, 'login.do')
+STISYS_RESULTS_URL = urljoin(STISYS_BASE_URL, 'viewExaminationData.do')
 
 
 class StisysWatcher:
-
-    login_data = {
-        'username': None,
-        'password': None
-    }
-
-    silent_mode = False
-
-    session = requests.Session()
-
-    difffile_path = 'stisys-watcher.tmp'
-
     def __init__(self):
+        # Create Empty dict
+        self.login_data = {}
+
+        self.silent_mode = False
+
+        self.session = requests.Session()
+
+        self.difffile_path = 'stisys-watcher.tmp'
+
         self.parse_cli_arguments()
         self.read_logindata()
         self.login()
@@ -28,10 +37,30 @@ class StisysWatcher:
             print(fresh_results)
 
     def parse_cli_arguments(self):
-        parser = argparse.ArgumentParser(description='Stisys Watcher. Automatically pull new results from Stisys (HAW Hamburg).')
-        parser.add_argument('-u', type=str, help='Your HAW username (a-idenfitier)', dest='username')
-        parser.add_argument('-p', type=str, help='Your HAW password', dest='password')
-        parser.add_argument('-s', action='store_true', help='Silent mode. Suppresses output apart from result.', dest='silent_mode')
+        parser = argparse.ArgumentParser(
+                description=(
+                    'Stisys Watcher. '
+                    'Automatically pull new results from Stisys (HAW Hamburg).'
+                    )
+                )
+
+        parser.add_argument(
+                '-u',
+                type=str,
+                help='Your HAW username (a-idenfitier)',
+                dest='username')
+
+        parser.add_argument(
+                '-p',
+                type=str,
+                help='Your HAW password',
+                dest='password')
+
+        parser.add_argument(
+                '-s',
+                action='store_true',
+                help='Silent mode. Suppresses output apart from result.',
+                dest='silent_mode')
 
         cli_args = parser.parse_args()
         self.login_data = {
@@ -41,27 +70,44 @@ class StisysWatcher:
         self.silent_mode = cli_args.silent_mode
 
     def read_logindata(self):
-        if not self.silent_mode: print('Stisys-Watcher')
+        if not self.silent_mode:
+            print('Stisys-Watcher')
 
         if self.login_data['username'] is None:
             self.login_data['username'] = input('Username: ')
         else:
-            if not self.silent_mode: print('Username: ' + str(self.login_data['username']))
+            if not self.silent_mode:
+                print('Username: ' + str(self.login_data['username']))
 
         if self.login_data['password'] is None:
             self.login_data['password'] = getpass.getpass('Password: ')
         else:
-            if not self.silent_mode: print('Password: Set via CLI argument.')
+            if not self.silent_mode:
+                print('Password: Set via CLI argument.')
 
     def login(self):
-        self.session.post('https://stisys.haw-hamburg.de/login.do', data=self.login_data)
+        resp = self.session.post(STISYS_LOGIN_URL, data=self.login_data)
+
+        # Check HTTP status code
+        resp.raise_for_status()
+
+        # Check if login failed
+        if 'Login' in resp.text and 'fehlgeschlagen' in resp.text:
+            raise Exception('Login Failed')
 
     def get_all_results(self) -> str:
-        result_html = self.session.get('https://stisys.haw-hamburg.de/viewExaminationData.do')
+        result_html = self.session.get(STISYS_RESULTS_URL)
 
         # Parse html result and find tables that contain the results
         parsed_html = BeautifulSoup(result_html.text, 'lxml')
-        result_tables = parsed_html.body.find('div', attrs={'id': 'ergebnisuebersicht'}).find_all('table', attrs={'class': 'tablecontent'})
+        div_container = parsed_html.body.find(
+                'div',
+                attrs={'id': 'ergebnisuebersicht'}
+            )
+        result_tables = div_container.find_all(
+                'table',
+                attrs={'class': 'tablecontent'}
+            )
 
         # Build pretty results
         all_results = ''
@@ -69,15 +115,22 @@ class StisysWatcher:
             result_rows = cur_table.find('table').find_all('tr')
             for cur_result_row in result_rows[1:]:
                 cells = cur_result_row.find_all('td')
-                all_results += '{} - {} - {} - {}\n'.format(' '.join(cells[1].text.split()), ' '.join(cells[3].text.split()), ' '.join(cells[5].text.split()), ' '.join(cells[7].text.split()))
+                all_results += '{} - {} - {} - {}\n'.format(
+                        cl_text(cells[1]),
+                        cl_text(cells[3]),
+                        cl_text(cells[5]),
+                        cl_text(cells[7])
+                    )
 
         return all_results
 
     def check_for_changes(self, new_results: str) -> bool:
-        self.create_difffile_if_nonexisting()
+        if path.isfile(self.difffile_path):
+            with open(self.difffile_path, 'r') as difffile:
+                old_results = difffile.read()
 
-        with open(self.difffile_path, 'r') as difffile:
-            old_results = difffile.read()
+        else:
+            old_results = ""
 
         if new_results != old_results:
             with open(self.difffile_path, 'w') as difffile:
@@ -86,12 +139,20 @@ class StisysWatcher:
 
         return False
 
-    def create_difffile_if_nonexisting(self):
-        try:
-            difffile = open(self.difffile_path, 'r')
-        except FileNotFoundError:
-            difffile = open(self.difffile_path, 'w+')
-        difffile.close()
+
+def cl_text(element):
+    """
+    Converts tree element to text and collapse Whitespaces
+
+    Yes the name is not exactly good,
+    but a longer one would partially betray the purpose of this function.
+
+    :type element: str
+    :rtype: str
+    """
+    return ' '.join(element.text.split())
 
 
-StisysWatcher()
+# simulate main function to allow import of this file
+if __name__ == '__main__':
+    StisysWatcher()
