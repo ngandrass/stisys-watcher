9c572c60ecf3280d56d7f8c95fc55dac9b76129d
ngandrass <ngandrass@squacu.de>
2018-01-30 13:10:48 +0100

    Class refactoring

--- stisys-watcher.py
+++ stisys-watcher.py
@@ -2,29 +2,45 @@ import requests
 import getpass
 from bs4 import BeautifulSoup
 
-print("Stisys-Watcher")
-username = input("Username: ")
-password = getpass.getpass("Password: ")
 
-loginData = {
-    'username': username,
-    'password': password
-}
+class StisysWatcher:
 
-session = requests.Session()
-session.post("https://stisys.haw-hamburg.de/login.do", data=loginData)
-result_html = session.get("https://stisys.haw-hamburg.de/viewExaminationData.do")
+    loginData = {
+        'username': None,
+        'password': None
+    }
 
-# Parse html result and find tables that contain the results
-parsed_html = BeautifulSoup(result_html.text, 'lxml')
-result_tables = parsed_html.body.find('div', attrs={'id': 'ergebnisuebersicht'}).find_all('table', attrs={'class': 'tablecontent'})
+    session = requests.Session()
 
-# Build pretty results
-all_results = ""
-for curTable in result_tables:
-    resultRows = curTable.find('table').find_all('tr')
-    for curResultRow in resultRows[1:]:
-        cells = curResultRow.find_all('td')
-        all_results += "{} - {} - {} - {}\n".format(" ".join(cells[1].text.split()), " ".join(cells[3].text.split()), " ".join(cells[5].text.split()), " ".join(cells[7].text.split()))
+    def __init__(self):
+        self.read_logindata()
+        self.login()
+        print(self.get_all_results())
 
-print(all_results)
+    def read_logindata(self):
+        print('Stisys-Watcher')
+        self.loginData['username'] = input('Username: ')
+        self.loginData['password'] = getpass.getpass('Password: ')
+
+    def login(self):
+        self.session.post('https://stisys.haw-hamburg.de/login.do', data=self.loginData)
+
+    def get_all_results(self) -> str:
+        result_html = self.session.get('https://stisys.haw-hamburg.de/viewExaminationData.do')
+
+        # Parse html result and find tables that contain the results
+        parsed_html = BeautifulSoup(result_html.text, 'lxml')
+        result_tables = parsed_html.body.find('div', attrs={'id': 'ergebnisuebersicht'}).find_all('table', attrs={'class': 'tablecontent'})
+
+        # Build pretty results
+        all_results = ''
+        for cur_table in result_tables:
+            result_rows = cur_table.find('table').find_all('tr')
+            for cur_result_row in result_rows[1:]:
+                cells = cur_result_row.find_all('td')
+                all_results += '{} - {} - {} - {}\n'.format(' '.join(cells[1].text.split()), ' '.join(cells[3].text.split()), ' '.join(cells[5].text.split()), ' '.join(cells[7].text.split()))
+
+        return all_results
+
+
+StisysWatcher()
